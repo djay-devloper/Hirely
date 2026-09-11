777480f920670c52bcc60ce27e20303baec5cf87
Dhananjay Yadav <djay.developer@gmail.com>
2026-09-11 16:50:24 +0530

    fixed CORS option

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
+    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
     credentials:true
 }
 
